af33a2049b5710f22be90e693cc648ccfd79a9b1
Erik Segura <esmtg84@gmail.com>
2025-09-02 11:00:21 -0500

    next steps completed

--- Start/Ch5/formatting_start.py
+++ Start/Ch5/formatting_start.py
@@ -9,12 +9,13 @@ from datetime import datetime
 
 # Times and dates can be formatted using a set of predefined string
 # control codes 
+now = datetime.now()
 
 
 #### Date Formatting ####
 
 # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
+print(now.strftime(""))
 
 # %c - locale's date and time, %x - locale's date, %X - locale's time
 
